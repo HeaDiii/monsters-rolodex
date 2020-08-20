e8ff60ca94f4728e9c90d151c9f8af4eb1bc099d
Marcel <kickinyaface@web.de>
2020-08-20 21:02:35 +0200

    Updates

---
+++ precache-manifest.8cdf74bd847c930de304365df87c4afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e648b117bcb82ab37f9a3e2ba512b5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "02081a4d030460860a3a",
+    "url": "/monsters-rolodex/static/css/main.a3b82004.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02081a4d030460860a3a",
+    "url": "/monsters-rolodex/static/js/main.3050d44a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
